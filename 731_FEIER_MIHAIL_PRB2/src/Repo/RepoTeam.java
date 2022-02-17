7c285d1e3a25e1813fae0bdfd7c9120653518232
fmg2000 <mihail.feierg@gmail.com>
2022-02-17 11:14:41 +0200

    Commit 2 RepoMiterbeiten

--- 731_FEIER_MIHAIL_PRB2/src/Repo/RepoTeam.java
+++ 731_FEIER_MIHAIL_PRB2/src/Repo/RepoTeam.java
@@ -1,4 +1,59 @@
 package Repo;
 
+import Model.Mitarbeiten;
+import Model.Team;
+
+import javax.sound.midi.Soundbank;
+import java.util.List;
+import java.util.Scanner;
+
 public class RepoTeam {
+
+    private List<Team> listTeam;
+    private Scanner sc = new Scanner(System.in);
+
+    public RepoTeam(List<Team> listTeam) {
+        this.listTeam = listTeam;
+    }
+
+    public List<Team> getListTeam() {
+        return listTeam;
+    }
+
+    public void setListTeam(List<Team> listTeam) {
+        this.listTeam = listTeam;
+    }
+
+    public void addTeam(Team item)
+    {
+        listTeam.add(item);
+    }
+    public void updateTeam(List<Mitarbeiten> mitarbeiten)
+    {
+        System.out.println("update team name: ");
+        for (Team i : listTeam)
+            if(i.getProjectname().equals(sc.next())) {
+                System.out.println("Name update");
+                i.setName(sc.next());
+                System.out.println("Projectname update: ");
+                i.setProjectname(sc.next());
+                System.out.println("List mitarbeiten");
+                i.setListMitarbeiten(mitarbeiten);
+            }
+    }
+
+    public void deleteTeam()
+    {
+        System.out.println("delete team \n ProjectName ");
+        for (Team i : listTeam)
+            if(i.getProjectname().equals(sc.next()))
+                listTeam.remove(i);
+    }
+
+    @Override
+    public String toString() {
+        return "RepoTeam{" +
+                "listTeam=" + listTeam +
+                '}';
+    }
 }
--- 731_FEIER_MIHAIL_PRB2/src/com/company/Main.java
+++ 731_FEIER_MIHAIL_PRB2/src/com/company/Main.java
@@ -1,8 +1,42 @@
 package com.company;
 
+import Model.Mitarbeiten;
+import Model.Team;
+import Repo.RepoMitarbeiten;
+import Repo.RepoTeam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Mitarbeiten p1 = new Mitarbeiten("aaa", 12);
+        Mitarbeiten p2 = new Mitarbeiten("baa", 16);
+        Mitarbeiten p3 = new Mitarbeiten("bbbb", 23242);
+
+        List<Mitarbeiten> listMitarbeiten = new ArrayList<>();
+        listMitarbeiten.add(p1);
+        listMitarbeiten.add(p3);
+        listMitarbeiten.add(p2);
+        RepoMitarbeiten rp1 = new RepoMitarbeiten(listMitarbeiten);
+
+        List<Mitarbeiten> listMiarbeiten1 = new ArrayList<>();
+        listMiarbeiten1.add(p1);
+        listMiarbeiten1.add(p3);
+
+        Team b1 = new Team("Salut", "Str asas", listMitarbeiten);
+        Team b2 = new Team("Hello", "asdasda", listMiarbeiten1);
+
+        List<Team> listTeam = new ArrayList<>();
+        listTeam.add(b1);
+        listTeam.add(b2);
+        RepoTeam rb1 = new RepoTeam(listTeam);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println(rb1.getListTeam());
     }
 }
